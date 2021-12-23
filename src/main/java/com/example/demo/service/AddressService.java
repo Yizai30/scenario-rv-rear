38b2e366a21ba18a2ddd2015dae10e63b0711bdf
VI-qiezi <1124629306@qq.com>
2021-12-23 17:17:16 +0800

    some updates

--- src/main/java/com/example/demo/service/AddressService.java
+++ src/main/java/com/example/demo/service/AddressService.java
@@ -5,11 +5,11 @@ public class AddressService {
 //	public static String userAddress = "/Users/vicky/Documents/SG/UserProject/";
 //	public static String dataAddress = "/Users/vicky/Documents/SG/Data/program/case/";
 	
-	public static String rootAddress = "C:/SG/Project/";
-	public static String userAddress = "C:/SG/UserProject/";
-	public static String dataAddress = "D:/Data/program/case/";
+//	public static String rootAddress = "C:/SG/Project/";
+//	public static String userAddress = "C:/SG/UserProject/";
+//	public static String dataAddress = "D:/Data/program/case/";
 
-//	public static String rootAddress = "/root/Env/Project/";
-//	public static String userAddress = "/root/Env/UserProject/";
-//	public static String dataAddress = "/root/Env/Data/program/case/";
+	public static String rootAddress = "/root/Env/Project/";
+	public static String userAddress = "/root/Env/UserProject/";
+	public static String dataAddress = "/root/Env/Data/program/case/";
 }
